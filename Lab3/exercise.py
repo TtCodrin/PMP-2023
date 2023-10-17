4944141e24a0b9b5fdf6666335063d627838877c
TtCodrin <126013263+TtCodrin@users.noreply.github.com>
2023-10-17 01:42:58 -0700

    Update exercise.py

--- Lab3/exercise.py
+++ Lab3/exercise.py
@@ -9,6 +9,6 @@ model = BayesianNetwork([('Incendiu','Alarma'), ('Cutremur','Alarma'),('Declansa
 cpd_cutremur = TabularCPD(variable="Cutremur", variable_card = 2, values=[[0.95],[0.05]])
 cpd_incendiu = TabularCPD(variable="Incendiu", variable_card=2, values = [0.99],[0.01], evidence="Cutremur", evidence_card=[2])
 cpd_declansare_accidentala_alarma = TabularCPD(variable="DeclansareAccidentalaAlarma", variable_card=2, values= [[0.99],[0.01]])
-cpd_alarma=TabularCPD(variable="Alarma", variable_card=2, values=[0.99, 0.98, 0.05, 0.02],[0.01,0.02,0.95,0.98], evidence=["Cutremur","Incendiu","DeclansareAccidentalaAlarma", evidence_card=[2,2,2]])
+cpd_alarma=TabularCPD(variable="Alarma", variable_card=2, values=[0.99, 0.98, 0.05, 0.02],[0.01,0.02,0.95,0.98], evidence=["Cutremur","Incendiu","DeclansareAccidentalaAlarma"], evidence_card=[2,2,2]])
 
 model.add__cpds(cpd_cutremur, cpd_incendiu, cod_declansare_accidentala_alarma, cpd_alarma)
